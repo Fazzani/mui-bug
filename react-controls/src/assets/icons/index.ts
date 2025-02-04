ed03f018c33f81e472629206739a15d3fc421020
Heni Fazzani <heni.fazzani.ext@up.coop>
2025-02-05 00:14:40 +0100

    Success

--- react-controls/src/assets/icons/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './LoadingIcon';
-export * from './CheckedCircle';
-export * from './UnCheckedCircle';
-export * from './UserSmileIcon';
---
+++ react-controls/src/index.tsx
@@ -0,0 +1,6 @@
+import './types/@mui';
+
+export { Button } from "./components/Button/Button";
+export type { ButtonProps, ButtonColors, ButtonSizes } from "./components/Button/Button";
+export * from './themes/defaultTheme';
+export { ThemeProvider, type ThemeProviderProps } from './components/ThemeProvider/ThemeProvider';
